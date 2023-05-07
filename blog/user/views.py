264ba56403eba6fc83a7c8fed89491859a14d998
Aliaksei <a.tabolitch@yandex.by>
2023-05-07 11:07:22 +0200

    Create new files: ../report/__init__.py, ../report/views.py, __pycache__/__init__.cpython-310.pyc, __pycache__/views.cpython-310.pyc

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,3 +1,14 @@
 from flask import Blueprint
 
-user = Blueprint('user', __name__, )
+user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
+
+
+@user.route('/')
+def user_list():
+    return 'Hello, user!'
+
+
+@user.route('</pk>')
+def get_user(pk: int):
+    return pk
+
